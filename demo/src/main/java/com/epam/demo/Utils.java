c9990d03232f7614c831de8f9ca244e599d67435
Vadimzakreva <vadim@sinai.io>
2023-02-02 18:59:07 +0200

    one more fixing

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
+        if (args == null) return false;
         for (String str : args) {
             try {
                 if (Float.parseFloat(str) <= 0) return false;
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -3,7 +3,7 @@ package com.epam.utils;
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
         try {
-            return (Float.parseFloat(str) < 0);
+            return (Float.parseFloat(str) > 0);
         } catch (NumberFormatException e) {
             return false;
         }
