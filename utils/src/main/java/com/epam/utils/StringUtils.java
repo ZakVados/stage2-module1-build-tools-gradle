98224bde7b3f7e2299ec77d19a78e85275f4b38d
Vadimzakreva <vadim@sinai.io>
2023-02-02 14:28:16 +0200

    init commit

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,7 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-         //here magic will happen
+        boolean res = Integer.parseInt(str) >= 0;
+        return res;
     }
 }
