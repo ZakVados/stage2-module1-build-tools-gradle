2ed08d9950a6c5be173e79fbc6fe641244629e65
Vadimzakreva <vadim@sinai.io>
2023-02-04 20:03:47 +0200

    fixed relationships

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -2,16 +2,18 @@ package com.epam.demo;
 
 import java.util.List;
 
+import static com.epam.utils.StringUtils.isPositiveNumber;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
         if (args == null) return false;
         for (String str : args) {
-            try {
-                if (Float.parseFloat(str) <= 0) return false;
-            } catch (NumberFormatException | NullPointerException e) {
-                return false;
-            }
+            if (! isPositiveNumber(str)) return false;
+//            try {
+//                if (Float.parseFloat(str) <= 0) return false;
+//            } catch (NumberFormatException | NullPointerException e) {
+//                return false;
+//            }
         }
         return true;
     }
