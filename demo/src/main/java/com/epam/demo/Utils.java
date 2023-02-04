55e91c0117101d2ba5c4ba1c3c453e982a978f00
Vadimzakreva <vadim@sinai.io>
2023-02-04 20:05:38 +0200

    fixed logic

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -8,12 +8,9 @@ public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
         if (args == null) return false;
         for (String str : args) {
-            if (! isPositiveNumber(str)) return false;
-//            try {
-//                if (Float.parseFloat(str) <= 0) return false;
-//            } catch (NumberFormatException | NullPointerException e) {
-//                return false;
-//            }
+            if (isPositiveNumber(str) == false) {
+                return false;
+            };
         }
         return true;
     }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,10 +1,19 @@
 package com.epam.utils;
 
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.regex.Pattern;
+
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
+        if (str == null) return false;
+        Pattern pattern = Pattern.compile("0\\d+");
+        if (pattern.matcher(str).matches()) return false;
+        NumberFormat format = NumberFormat.getInstance();
         try {
-            return (Float.parseFloat(str) > 0);
-        } catch (NumberFormatException | NullPointerException e) {
+            Number value = format.parse(str);
+            return (value.floatValue() > 0);
+        } catch (ParseException e) {
             return false;
         }
 
